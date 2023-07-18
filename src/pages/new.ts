2887021a503bcf15e9d0bacaf4047f248b4a05c0
fazzaamiarso <fazzaamiarso@gmail.com>
2023-07-19 02:59:39 +0700

    refactor: refactor form validation

--- src/pages/new.ts
+++ src/pages/new.ts
@@ -9,8 +9,8 @@ class NewPage extends LitNoShadow {
     e.preventDefault();
     if (!(e.currentTarget instanceof HTMLFormElement)) return;
     this._validateForm(e.currentTarget);
-
     if (!this._isFormValid(e.currentTarget)) return;
+
     const formData = new FormData(e.currentTarget);
     const imageFile = formData.get("story-image");
     const description = formData.get("story-description");
@@ -20,22 +20,7 @@ class NewPage extends LitNoShadow {
   }
 
   private _validateForm(form: HTMLFormElement) {
-    const formData = new FormData(form);
-    Array.from(formData.entries()).forEach(([key]) => {
-      const inputElement = form.querySelector(`[name='${key}']`) as HTMLInputElement;
-      const errorElement = form.querySelector(`#${key}-error`) as HTMLElement;
-      if (inputElement.validity.valid) {
-        errorElement.classList.remove("invalid-feedback");
-        inputElement.classList.add("is-valid");
-        inputElement.classList.remove("is-invalid");
-        errorElement.innerText = "";
-      } else {
-        inputElement.classList.add("is-invalid");
-        inputElement.classList.remove("is-valid");
-        errorElement.classList.add("invalid-feedback");
-        errorElement.innerText = inputElement.validationMessage;
-      }
-    });
+    form.classList.add("was-validated");
   }
 
   private _isFormValid(form: HTMLFormElement) {
@@ -49,12 +34,16 @@ class NewPage extends LitNoShadow {
         <div class="mb-3">
           <label for="story-image" class="form-label">Upload image</label>
           <input class="form-control" name="story-image" type="file" id="story-image" accept="image/*" required />
-          <p id="story-image-error" class=""></p>
+          <p class="valid-feedback">Look's good!</p>
+          <p class="invalid-feedback">Please upload an image</p>
+          <!-- <p id="story-image-error" class=""></p> -->
         </div>
         <div class="mb-3">
           <label for="story-description" class="form-label">Description</label>
           <textarea class="form-control" name="story-description" id="story-description" required></textarea>
-          <p id="story-description-error" class=""></p>
+          <p class="valid-feedback">Look's good!</p>
+          <p class="invalid-feedback">Please provide a valid description</p>
+          <!-- <p id="story-description-error" class=""></p> -->
         </div>
         <button class="btn btn-primary">Post Story</button>
       </form>
